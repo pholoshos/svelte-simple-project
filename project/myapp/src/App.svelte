2bf3e712f4605451062c998f53fd53d67c76e71d
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2022-08-24 03:06:18 +0200

    svelte example

---
+++ project/myapp/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  import Home from './lib/Home.svelte'
+
+  let number =  10;
+</script>
+
+<main>
+  <Home pageName="good food"></Home>
+</main>
+
+<style>
+
+</style>
---
+++ project/myapp/src/lib/Home.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {fade,fly} from 'svelte/transition'
+    export let pageName  = "GOod page"
+    let hasAdded = false;
+    
+    let names = [1,2,3]
+
+    const changeName = ()=>{
+        pageName = "we are changed"
+        hasAdded = true;
+        names.push(4);
+        console.log("LOG ::: names",names)
+    }
+</script>
+
+<div>
+    <h1 transition:fly>Home Page : {pageName}</h1>
+    <input placeholder="Enter title here"  bind:value={pageName}/>
+    {#if hasAdded === true}
+        <h1 transition:fly>Home Page : {pageName}</h1>
+    {/if}
+    {#each names as name}
+        <h3 transition:fly>{name}</h3>
+    {/each}
+    <button on:click={changeName}>Add Stuff</button>
+
+</div>
